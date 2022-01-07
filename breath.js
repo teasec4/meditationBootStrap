f71af8d25e75fd7db721c7c502c6773a382e25bb
teasec4 <85108614+teasec4@users.noreply.github.com>
2022-01-07 18:50:14 +0800

    Add files via upload

--- breath.js
+++ breath.js
@@ -1,5 +1,8 @@
 let timerIn = document.getElementById('timerIn');
 
+colorinhale = '#198754';
+colorexhale = '#dc3545' ;
+
 button2.addEventListener('click', function () {
     secondsIn = 1
 
@@ -9,42 +12,42 @@ timer = setInterval(function () {
 
     if(document.getElementById('option1').checked === true){
         if(secondsIn >=1 && secondsIn<6){
-
+        timerIn.style.color = colorinhale
 
         }
         if (secondsIn >=6 && secondsIn<16){
-
+            timerIn.style.color = colorexhale
         }
         if (secondsIn >=16){
             secondsIn=1
-
+            timerIn.style.color = colorinhale
         }
 
     }
 
     if (document.getElementById("option2").checked === true){
         if(secondsIn >=1 && secondsIn<9){
-
+            timerIn.style.color = colorinhale
         }
         if (secondsIn >=9 && secondsIn<24){
-
+            timerIn.style.color = colorexhale
         }
         if (secondsIn >=24){
             secondsIn=1
-
+            timerIn.style.color = colorinhale
         }
     }
 
     if (document.getElementById("option3").checked === true) {
         if (secondsIn >= 1 && secondsIn < 11) {
-
+            timerIn.style.color = colorinhale
         }
         if (secondsIn >= 11 && secondsIn < 31) {
-
+            timerIn.style.color = colorexhale
         }
         if (secondsIn >= 31) {
             secondsIn = 1
-
+            timerIn.style.color = colorinhale
         }
     }
 

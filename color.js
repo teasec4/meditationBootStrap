6f120de0023b254ff47a92301fe0c62686ce18be
teasec4 <85108614+teasec4@users.noreply.github.com>
2022-01-30 20:51:23 +0800

    Add files via upload

--- color.js
+++ color.js
@@ -3,8 +3,8 @@ let navColor = document.getElementById('nav')
 let buttonClose = document.getElementById('buttonClose')
 
 button2.addEventListener('click', function () {
-    bodyColor.className = "bg-dark";
-    navColor.className = "navbar navbar-expand-md navbar-dark fixed-top bg-dark";
+    bodyColor.className = "bg-light";
+    navColor.className = "navbar navbar-expand-md navbar-dark fixed-top bg-secondary";
 })
 
 buttonClose.addEventListener('click', function () {
